f6fd45bff51ea044d235626325a77319bf28b1ff
connoc5 <connoc5@rpi.edu>
2019-03-18 17:12:48 -0400

    Step one of Audio Manager

---
+++ Apple Catcher/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    #region Variables
+    public GameObject Manager;
+    private GameObject SongHolder = GameObject.Find("Song");
+    public AudioSource Audio;
+    public Slider Slider;
+    private bool Open = false;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OpenMenu()
+    {
+        if (Open == false)
+        {
+            Manager.SetActive(true);
+            Open = true;
+        }
+        else
+        {
+            Manager.SetActive(false);
+            Open = false;
+        }
+    }
+    public void SetVolume()
+    {
+        Audio.volume = Slider.value * 0.05f;
+    }
+}
